30b4bbc28f92ffe1aed2fb78d0958ff5ee2a7e75
Sam Welsh <samwelsh@hotmail.co.uk>
2026-01-17 21:05:09 +0000

    feat: Add Teko & Inter fonts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
 	},
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.google(),
+				name: "Teko",
+				cssVariable: "--font-teko"
+			},
+			{
+				provider: fontProviders.google(),
+				name: "Inter",
+				cssVariable: "--font-inter"
+			}
+		]
+	}
 });
